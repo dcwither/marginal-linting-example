5d261687f2b0b8b73b493c2b0f848c9d030c5332
Devin Witherspoon <devin.witherspoon@chanzuckerberg.com>
2020-11-12 17:18:32 -0800

    changes to highlight reviewdog's annotations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import './App.css';
+
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
@@ -8,7 +9,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
